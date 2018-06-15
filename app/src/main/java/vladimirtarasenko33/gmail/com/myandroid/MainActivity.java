4270cad9a2e3dce288b2b3d0edf2d5b49b7e350b
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2018-06-15 14:27:53 +0300

    Dz_2
    Two button menu(MainActivity) and flags (SecondActivity)

--- app/src/main/java/vladimirtarasenko33/gmail/com/myandroid/MainActivity.java
+++ app/src/main/java/vladimirtarasenko33/gmail/com/myandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package vladimirtarasenko33.gmail.com.myandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,52 +9,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener{
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        final TextView textView1 = (TextView) findViewById(R.id.textView2);
-        View button1 =  findViewById(R.id.textView2);
-        button1.setOnClickListener(this);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = (String) textView.getText();
-                textView.setText(textView1.getText());
-                textView1.setText(text);
-                ColorStateList color = textView.getTextColors();
-                textView.setTextColor(textView1.getTextColors());
-                textView1.setTextColor(color);
+
             }
-        });
+
+    public void OnClickDz1(View view) {
+        Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public void onClick(View v) {
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        final TextView textView1 = (TextView) findViewById(R.id.textView2);
-        View button1 =  findViewById(R.id.textView2);
-        String text = (String) textView.getText();
-        textView.setText(textView1.getText());
-        textView1.setText(text);
-        ColorStateList color = textView.getTextColors();
-        textView.setTextColor(textView1.getTextColors());
-        textView1.setTextColor(color);
-    }
-
-    public void onMyClick(View view) {
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        final TextView textView1 = (TextView) findViewById(R.id.textView2);
-        String text = (String) textView.getText();
-        textView.setText(textView1.getText());
-        textView1.setText(text);
-        ColorStateList color = textView.getTextColors();
-        textView.setTextColor(textView1.getTextColors());
-        textView1.setTextColor(color);
+    public void OnClickDz2(View view) {
+        Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
