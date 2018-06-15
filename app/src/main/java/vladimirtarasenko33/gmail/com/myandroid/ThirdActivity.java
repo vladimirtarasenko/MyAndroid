cff91183c5b1a53bad1c8e0203a5ac1647e979e2
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2018-06-15 14:33:25 +0300

    DZ_3
    ImageView in ThirdActivity

--- app/src/main/java/vladimirtarasenko33/gmail/com/myandroid/ThirdActivity.java
+++ app/src/main/java/vladimirtarasenko33/gmail/com/myandroid/ThirdActivity.java
@@ -1,13 +1,28 @@
 package vladimirtarasenko33.gmail.com.myandroid;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class ThirdActivity extends Activity {
+
+    ImageView imageView;
+    EditText newURL;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+       imageView =(ImageView) findViewById(R.id.imageView);
+        newURL=findViewById(R.id.edit);
+    }
+
+    public void ImageClick(View view) {
+       Picasso.get().load(String.valueOf(newURL.getText())).into(imageView);
     }
 }
